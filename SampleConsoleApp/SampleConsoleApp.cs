8f71fcc2cc0dc56ecf94baae31e4db4bebea9f65
Dustin Jones <DUJones@esd.wa.gov>
2018-09-20 09:20:40 -0700

    Initial API sample application

---
+++ SampleConsoleApp/SampleConsoleApp.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleFilingApiClient;
+
+namespace SampleConsoleApp
+{
+    class SampleConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("API URL: https://ess-api-management.azure-api.net/file/api/");
+            string url = " https://ess-api-management.azure-api.net/file/api/"; // Console.ReadLine();
+            Console.Write("Subscription Key: ");
+            string subscriptionKey = Console.ReadLine();
+            ApiClient client = new ApiClient(url, subscriptionKey);
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Sample Application Menu:");
+                Console.WriteLine("1. Check tax rate");
+                Console.WriteLine("2. File quarterly tax report");
+                Console.WriteLine("3. Check tax report status");
+                Console.WriteLine("0. Exit");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        CheckTaxRate(client);
+                        break;
+                    case "2":
+                        FileQuarterlyReport(client);
+                        break;
+                    case "3":
+                        CheckTaxReportStatus(client);
+                        break;
+                    case "0":
+                        loop = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void CheckTaxRate(ApiClient client)
+        {
+            try
+            {
+                Console.Write("ESD Number: ");
+                string esdNumber = Console.ReadLine();
+                Console.Write("UBI Number: ");
+                string ubiNumber = Console.ReadLine();
+                Console.Write("FEIN Number: ");
+                string feinNumber = Console.ReadLine();
+                Console.Write("Month (as number): ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                string result = client.GetTaxRate(esdNumber, ubiNumber, feinNumber, month, year);
+                Console.WriteLine(result);
+            }
+            catch { }
+
+        }
+
+        private static void FileQuarterlyReport(ApiClient client)
+        {
+            Console.Write("Quarterly Report: ");
+            string previousLine = string.Empty;
+            string report = string.Empty;
+            bool read = true;
+            while (read)
+            {
+                string currentLine = Console.ReadLine();
+                if (currentLine == string.Empty && previousLine == string.Empty)
+                    read = false;
+                else
+                {
+                    previousLine = currentLine;
+                    report += currentLine + Environment.NewLine;
+                }
+            }
+            string result = client.Submit(report.Substring(0, report.Length - 4));
+            Console.WriteLine($"Tracking ID: {result}");
+        }
+
+        private static void CheckTaxReportStatus(ApiClient client)
+        {
+            Console.Write("Tracking ID: ");
+            string trackingId = Console.ReadLine();
+            string result = client.GetTaxAndWageReportStatus(trackingId);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SampleFilingApiClient/SampleFilingApiClient.cs
@@ -0,0 +1,84 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFilingApiClient
+{
+    public class ApiClient
+    {
+        private static string _subscriptionkey;
+        private static RestClient _client;
+
+        /// <summary>
+        /// Sample API Client for ESD Filing API.
+        /// </summary>
+        /// <param name="baseUrl">Base URL for api calls.</param>
+        /// <param name="subscriptionKey">Subscription key for authorization.</param>
+        public ApiClient(string baseUrl, string subscriptionKey)
+        {
+            if (string.IsNullOrWhiteSpace(baseUrl)) throw new ArgumentException("Parameter `baseUrl` cannot be null or empty.");
+            if (string.IsNullOrWhiteSpace(subscriptionKey)) throw new ArgumentException("Parameter `subscriptionKey` cannot be null or empty.");
+
+            _client = new RestClient(baseUrl);
+            _subscriptionkey = subscriptionKey;
+        }
+
+        /// <summary>
+        /// Submits request.
+        /// </summary>
+        private string SubmitRequest(RestRequest request)
+        {
+            request.AddHeader("Ocp-Apim-Subscription-Key", _subscriptionkey);
+            IRestResponse response = _client.Execute(request);
+
+            return response.Content;
+        }
+
+        /// <summary>
+        /// FilingStatus_GetTaxAndWageReportStatus.
+        /// </summary>
+        public string GetTaxAndWageReportStatus(string trackingId)
+        {
+            if (string.IsNullOrWhiteSpace(trackingId)) throw new ArgumentException("Parameter `trackingId` cannot be null or empty");
+
+            RestRequest request = new RestRequest($"status/tax-and-wage-report/{trackingId}", Method.POST);
+
+            return SubmitRequest(request);
+        }
+
+        /// <summary>
+        /// TaxAndWageFiling_SubmitAsync
+        /// </summary>
+        public string Submit(string filingJson)
+        {
+            if (string.IsNullOrWhiteSpace(filingJson)) throw new ArgumentException("Parameter `filingJson` cannot be null or empty");
+
+            RestRequest request = new RestRequest($"file/single", Method.POST);
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/json", filingJson, ParameterType.RequestBody);
+            string result = SubmitRequest(request);
+
+            return result.Substring(1, result.Length - 2);
+        }
+
+        /// <summary>
+        /// TaxAndWageInfo_GetTaxRate
+        /// </summary>
+        public string GetTaxRate(string esdNumber, string ubiNumber, string feinNumber, int month, int year)
+        {
+            if (string.IsNullOrWhiteSpace(esdNumber)) throw new ArgumentException("Parameter `esdNumber` cannot be null or empty");
+            if (string.IsNullOrWhiteSpace(ubiNumber)) throw new ArgumentException("Parameter `ubiNumber` cannot be null or empty");
+            if (string.IsNullOrWhiteSpace(feinNumber)) throw new ArgumentException("Parameter `feinNumber` cannot be null or empty");
+            if (month < 1 || month > 12) throw new ArgumentException("Parameter `month` must be between 1 and 12 inclusive");
+            if (year < 0) throw new ArgumentException("Parameter `year` must not be negative");
+
+            RestRequest request = new RestRequest($"info/tax-rate/{esdNumber}/{ubiNumber}/{feinNumber}/{month}/{year}", Method.GET);
+
+            return SubmitRequest(request);
+        }
+    }
+}
